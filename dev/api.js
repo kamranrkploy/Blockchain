a46b28fbfb6b368ea5000fe5014a86f8f8bb748a
kamranrk <kamran16alam@gmail.com>
2020-08-08 20:57:27 +0530

    installed body parser to our project and modified api.js

--- dev/api.js
+++ dev/api.js
@@ -1,6 +1,14 @@
 var express = require('express');
 const port = 3010;
-var app = express();
+const bodyParser = require('body-parser');
+const app = express();
+
+//to use body parser library
+//basically these two lines says if a request comes in with json data or with form data we simply wanna pass that data so that we can access it at any of the routes
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : false}));
+
+
 
 //sends back to us our entire blockchain
 app.get('/blockchain' , function(req , res){
@@ -9,7 +17,8 @@ app.get('/blockchain' , function(req , res){
 
 //for creating a new transaction for our blockchain
 app.post('/transaction' , function(req , res){
-    
+    console.log(req.body);
+    res.send(`the amount of transaction is ${req.body.amount} Zypher`);
 });
 
 //it ll going to mine  a new block for us

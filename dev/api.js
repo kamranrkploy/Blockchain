4791a8ca1ccaf71935a443433c64a67bff57bdb4
kamranrk <kamran16alam@gmail.com>
2020-08-10 20:15:27 +0530

    accessed our blockchain and Transaction methods using API

--- dev/api.js
+++ dev/api.js
@@ -1,7 +1,10 @@
-var express = require('express');
+const express = require('express');
 const port = 3010;
 const bodyParser = require('body-parser');
 const app = express();
+const Blockchain = require('./blockchain');
+
+const Zypher = new Blockchain();
 
 //to use body parser library
 //basically these two lines says if a request comes in with json data or with form data we simply wanna pass that data so that we can access it at any of the routes
@@ -12,7 +15,7 @@ app.use(bodyParser.urlencoded({extended : false}));
 
 //sends back to us our entire blockchain
 app.get('/blockchain' , function(req , res){
-    
+    res.send(Zypher);
 });
 
 //for creating a new transaction for our blockchain
@@ -20,8 +23,8 @@ app.get('/blockchain' , function(req , res){
 //select body , raw and select the format as json
 //now place your dummy data here , in the form of json object
 app.post('/transaction' , function(req , res){
-    console.log(req.body);
-    res.send(`the amount of transaction is ${req.body.amount} Zypher`);
+   const blockIndex = Zypher.createNewTransaction(req.body.amount , req.body.sender , req.body.reciever);
+   res.json({ note: `The transaction will be added to block ${blockIndex}`});
 });
 
 //it ll going to mine  a new block for us
